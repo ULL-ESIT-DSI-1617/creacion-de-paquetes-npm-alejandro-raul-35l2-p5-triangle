e9f6330809be58c258ef84adb66a0181bc157e86
AlejandrDiaz <alu0100761948@ull.edu.es>
2017-04-27 01:32:13 +0000

    añadiendo triangle y test

---
+++ test/test.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var Triangle = require('../lib/triangle.js');
+let Shape = require('@ull-alejandro-raul-35l2/ull-shape');
+require("should");
+
+describe("getArea", function() {
+  
+  it("must compute the triangle area correctly. Triangle object", function() {
+    let a = new Triangle({ width: 100, height: 50 });
+    let s = a.getArea();
+    s.should.equal(2500);
+  })
+});
