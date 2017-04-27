57305bb6395cae6d2f182abc8dd6a850b956da68
AlejandrDiaz <alu0100761948@ull.edu.es>
2017-04-27 02:33:56 +0000

    cambiando test

--- test/test.js
+++ test/test.js
@@ -6,6 +6,12 @@ require("should");
 
 describe("getArea", function() {
   
+  it("must compute the square shape area correctly", function(){
+		let a = new Shape ({ width: 100, height: 50 }, 'Triangle');
+		let s = a.getArea();
+		s.should.equal(2500);
+	});
+  
   it("must compute the triangle area correctly. Triangle object", function() {
     let a = new Triangle({ width: 100, height: 50 });
     let s = a.getArea();
